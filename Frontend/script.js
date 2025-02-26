6533f20241257fb51e1949d716097888a93890d2
191204taianh <anhnt.22bi13028@usth.edu.vn>
2025-02-26 13:25:12 +0700

    Updated

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,6 @@
-<script>
+{/* <script>
   function scrollToSearch() {
     document.getElementById("search-bar").scrollIntoView({ behavior: "smooth" });
     document.getElementById("search-bar").querySelector("input").focus(); // Focus on the search bar
   }
-</script>
+</script> */}
